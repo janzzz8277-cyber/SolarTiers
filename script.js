f7b440052a589a6a4e0857b6c581e1e80cee4488
Andria Janjulia <janzzz8277@gmail.com>
2026-08-03 20:45:03 +0400

    add

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const DEFAULT_PLAYERS = [
   {name:'LukaMamaci', region:'EU', skinUsername:'LukaMamaci', tiers:{vanilla:null, sword:'LT5', axe:null, uhc:null, pot:'LT5', nethpot:null, smp:null, mace:null}},
   {name:'DangerMarIow', region:'EU', skinUsername:'DangerMarIow', tiers:{vanilla:null, sword:'HT4', axe:null, uhc:null, pot:null, nethpot:'LT4', smp:null, mace:null}},
   {name:'tato_17', region:'EU', skinUsername:'tato_17', tiers:{vanilla:null, sword:'LT5', axe:null, uhc:null, pot:null, nethpot:null, smp:null, mace:'LT4'}},
+  {name:'hhurtfull', region:'EU', skinUsername:'hhurtfull', tiers:{vanilla:null, sword:'LT3', axe:'LT3', uhc:'LT3', pot:'LT3', nethpot:'HT3', smp:'HT3', mace:'LT3'}},
 ];
 
 
